9ac4f2d1f018e1700473580a35e3ff68889de1b2
Andrej Chudy <xchudy03@stud.fit.vutbr.cz>
2016-10-10 21:54:17 +0200

    Fix listen

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ io.on('connection', function(socket) {
 db.sequelize.sync({
     force: true
 }).then(function() {
-    app.listen(PORT, function() {
+    http.listen(PORT, function() {
         console.log('Express listening on port ' + PORT + '!');
     });
 });
